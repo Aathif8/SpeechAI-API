7e1a864d091ddc62abec00e4f323834c43b81c7c
Aathif26 <aathif0710@gmail.com>
2025-03-07 17:00:39 +0530

    Port Updated

--- main.py
+++ main.py
@@ -144,4 +144,4 @@ def process_rag(transcribed_text):
 
 # Run FastAPI server
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
