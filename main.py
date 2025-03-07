de7f9989dc4a4a98c62679f31587f8743374211e
Aathif26 <aathif0710@gmail.com>
2025-03-07 13:00:36 +0530

    port updated

--- main.py
+++ main.py
@@ -144,5 +144,4 @@ def process_rag(transcribed_text):
 
 # Run FastAPI server
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
