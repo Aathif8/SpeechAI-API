5bedc1c19aa2f020ffedbc4480ed1a60659635e7
Aathif26 <aathif0710@gmail.com>
2025-03-11 15:22:30 +0530

    llm Model updated

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import uvicorn
 import openai
 import tempfile
 import io
-import shutil
 from fastapi import FastAPI, UploadFile, File, HTTPException, BackgroundTasks
 from fastapi.responses import FileResponse
 from langchain_community.vectorstores import Chroma
@@ -17,48 +16,37 @@ from huggingface_hub import hf_hub_download
 from dotenv import load_dotenv
 from faster_whisper import WhisperModel
 
-# Load environment variables
-load_dotenv()
+# Initializing FastAPI app
+app = FastAPI()
+
 # OpenAI API Key
+load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
 #HuggingFaceHub Key
 HF_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
-# Initializing FastAPI app
-app = FastAPI()
-
 # Configuration
 # MODEL_PATH = "models/mistral-7b-instruct-v0.1.Q4_K_M.gguf"
-MODEL_PATH = hf_hub_download(
-    repo_id="Aathif/mistral-7b-instruct-v0.1.Q4_K_M.gguf", 
-    filename="mistral-7b-instruct-v0.1.Q4_K_M.gguf",
-    token=HF_TOKEN
-)
+MODEL_PATH = hf_hub_download(repo_id="Aathif/mistral-7b-instruct-v0.1.Q4_K_M.gguf", filename="tinyllama-1.1b-chat-v1.0.Q3_K_S.gguf", token=HF_TOKEN)
 CHROMA_DB_PATH = os.path.join(tempfile.gettempdir(), "chroma_db")
 
 
 # Load Mistral model
-llm = LlamaCpp(
-    model_path=MODEL_PATH, 
-    n_ctx=2048, 
-    n_threads=1, 
-    f16_kv=True, 
-    verbose=False
-)
+llm = LlamaCpp(model_path=MODEL_PATH, n_ctx=1024, n_threads=1, f16_kv=True, verbose=False)
 
 # Load model for Embedding
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 # Whisper model for Speech-to-Text
-model = WhisperModel("tiny")
+model = WhisperModel("small")
 
 # Global retriever
 retriever = None
 
 # Data Upload API
 @app.post("/upload_file")
-async def upload_file(background_tasks: BackgroundTasks, file: UploadFile = File(..., max_length=10*1024*1024)):
+async def upload_file(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     global retriever
 
     if not file:
@@ -66,7 +54,7 @@ async def upload_file(background_tasks: BackgroundTasks, file: UploadFile = File
 
     # Read file content into memory
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-        shutil.copyfileobj(file.file, temp_file)
+        temp_file.write(await file.read())
         temp_file_path = temp_file.name
 
     # Process file in the background
@@ -82,27 +70,19 @@ def process_file(temp_file_path):
     all_docs = pdf_loader.load()
 
     # Split into Chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     vectorstore = None
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=25)
     
     for doc in all_docs:
         split_docs = text_splitter.split_documents([doc])
         if vectorstore is None:
             # Store in ChromaDB
-            vectorstore = Chroma.from_documents(
-                split_docs, 
-                embeddings, 
-                persist_directory=CHROMA_DB_PATH, 
-                collection_name="pdf_embeddings"
-            )
+            vectorstore = Chroma.from_documents(split_docs, embeddings, persist_directory=CHROMA_DB_PATH, collection_name="pdf_embeddings")
         else:
             vectorstore.add_documents(split_docs)
 
     # Create retriever
     retriever = vectorstore.as_retriever()
-
-    # Clean up temp file to save memory
-    os.remove(temp_file_path)
 
 # Speech-to-Text Function
 def Transcribe(audio_bytes):
@@ -112,7 +92,7 @@ def Transcribe(audio_bytes):
     # return response.text
 
     audio_file = io.BytesIO(audio_bytes)
-    segments, _ = model.transcribe(audio_file, beam_size=1, vad_filter=True)
+    segments, _ = model.transcribe(audio_file, beam_size=1)
     return " ".join({segment.text for segment in segments})
 
 # Text-To-Speech Function
@@ -120,8 +100,7 @@ def generate_speech(text):
     response = openai.audio.speech.create(
         model="tts-1",
         voice="alloy",
-        input=text,
-        strem=True
+        input=text
     )
     #Save audio to temp file
     temp_audio_path = "output_audio.mp3"
@@ -130,14 +109,14 @@ def generate_speech(text):
     return temp_audio_path
 
 @app.post("/process_audio/")
-async def process_audio(file: UploadFile = File(..., max_length=10 * 1024 * 1024)):
+async def process_audio(file: UploadFile = File(...)):
     global retriever
 
     # Save the uploaded file
-    audio_stream = file.file
+    audio_bytes = await file.read()
 
     # Transcription of Audio
-    transcribed_text = Transcribe(audio_stream)
+    transcribed_text = Transcribe(audio_bytes)
 
     # Query LLM (RAG Model)
     if retriever is None:
@@ -165,6 +144,5 @@ def process_rag(transcribed_text):
 
 # Run FastAPI server
 if __name__ == "__main__":
-    port = os.getenv("PORT", 8080)
-    print(f"Running on port: {port}")
-    uvicorn.run(app, host="0.0.0.0", port=int(port))
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
